fc9e0ff1eddc41acd398b3079d5d83f1cb3977f2
AyomideF <70255224+AyomideF@users.noreply.github.com>
2022-06-25 10:08:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var number1 = parseInt (prompt('number1: '))
+var oper = prompt("operator: - / * +");
+var number2 =parseInt (prompt('number2: '))
+
+if (oper === '+') {alert(number1 + number2);
+    console.log (number1 + number2)}
+  else if (oper === '+') {alert(number1 + number2);
+   console.log (number1 + number2)}
+  else if (oper === '-') {alert(number1 - number2);
+    console.log (number1 - number2)}
+  else if (oper === '*') {alert(number1 * number2);
+    console.log (number1 * number2)}
+  else if (oper === '/') {alert(number1 / number2);
+    console.log (number1 / number2)}
+               
